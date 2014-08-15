f12e42cc6ca061d19c07f089a2bb955490b0c29d
zdwen <october731@163.com>
2014-08-15 19:40:38 +0800

    实现事件驱动机制的版本。当前存在连接时即重连的bug。业务正常。

--- socket-client/demo1.js
+++ socket-client/demo1.js
@@ -1,23 +1,45 @@
-var SocketEventClient = require("./libs/socketEventClient").SocketEventClient;
+var SocketEventClient = require("./lib/socketEventClient").SocketEventClient;
 
 var client = new SocketEventClient('http://127.0.0.1:2900/', 'WzdClient_Node');
+
+client.onSubscribeCompleted(subscribeHandler);
+client.onEnqueueCompleted(enqueueHandler);
+client.onConnectFailed(connectFailedHandler);
+client.onInnerError(innerErrorHandler);
+client.onEventArrived('PriceChanged', priceChangedHandler);
+client.onEventArrived('PublishSalesState', publishSalesStateHandler);
+
+
 
 setTimeout(doWork, 10*1000);
 
 function doWork(){
-	client.subscribe('PriceChanged', priceChangedHandler, operationCallback2);
-	client.subscribe('PublishSalesState', publishSalesStateHandler, operationCallback);
-	client.enqueue('PriceChanged', 1, 60, { 'ListingSku' : '5100444'}, operationCallback);
+	client.connect();
+
+	client.subscribe('PriceChanged');
+	client.subscribe('PublishSalesState');
+	client.enqueue('PriceChanged', 1, 60, { 'ListingSku' : '5100444'});
 };
 
 console.log('启动完成');
 
-// var client = new SocketEventClient('http://127.0.0.1:2900/', 'WzdClient_Node');
+function subscribeHandler(eventName){
+	console.log('----------------订阅事件成功:', eventName);
+};
+
+function enqueueHandler(eventName){
+	console.log('----------------推送事件成功:', eventName);
+};
+
+function connectFailedHandler(){
+	console.log('----------------初始化连接失败，理论上调用方应该在接收到这个事件之后不再做任何Subscribe或Enqueue操作');
+};
+
+function innerErrorHandler(){
+	console.log('----------------内部错误，理论上调用方应该在接收到这个事件之后不再做任何Subscribe或Enqueue操作');
+};
 
 
-// client.subscribe('PriceChanged', priceChangedHandler, operationCallback);
-// client.subscribe('PublishSalesState', publishSalesStateHandler, operationCallback);
-// client.enqueue('PriceChanged', 1, 60, { 'ListingSku' : '5100444'}, operationCallback);
 
 function priceChangedHandler(arg){
 	console.log('--------------------------------------The price of Sku【' + arg.ListingSku + '】 changed! It would be handled then.');
@@ -27,11 +49,3 @@ function publishSalesStateHandler(arg){
 	console.log('-----------+++++++++++++++-------------The salesState of Sku【' + arg.ListingSku + '】 changed! It would be handled then.');
 };
 
-function operationCallback(result){
-	console.log('+++++++++++++++++++++++++++++++++++操作结果为：' + JSON.stringify(result));
-};
-
-function operationCallback2(result){
-	console.log('+++++++++++++++++++++++++++++++++++操作结果为：' + JSON.stringify(result));
-	client.subscribe('PriceChanged', priceChangedHandler, operationCallback);
-};
--- socket-client/lib/socketEventClient.js
+++ socket-client/lib/socketEventClient.js
@@ -1,15 +1,19 @@
-var io = require('socket.io-client');
+var _io = require('socket.io-client');
+var _events = require('events');
+var _util = require("util");
 
 function SocketEventClient (serverHost, clientId) {
 	this.clientId = clientId;
-	this.eventDic = new Object();
+	this.events = [];
+	this.connectAttempts = 0;
+	this.reconnectAttempts = 0;
 
 	var options = {
 		'reconnect' : false
-		, 'auto connect': true
+		, 'auto connect': false
 	};
 
-	this.sckClient = io.connect(serverHost, options);
+	this.sckClient = _io.connect(serverHost, options);
 
 	this.sckClient.on('connect', function(arg1, arg2){
 		this.reSubscribeEvents();
@@ -20,11 +24,44 @@ function SocketEventClient (serverHost, clientId) {
 	}.bind(this));
 };
 
-SocketEventClient.prototype.reconnect = function() {
+_util.inherits(SocketEventClient, _events.EventEmitter);
+
+SocketEventClient.prototype.connect = function() {
+	if(this.connectAttempts++ > 60){
+		this.emit('connect_failed');
+		this.connectAttempts = 0;
+		//return;TODO【闻祖东 2014-8-15-155646】恐怕这个地方不能return，一return就又整个退出了。
+	}
+
 	var sct = this.sckClient.socket;
 
-	if (sct.connected || sct.reconnecting)
+	if (sct.reconnecting)
 		return;
+
+	if(sct.connected){
+		this.emit('connect');
+		return;
+	}
+
+	sct.connect();
+	sct.publish('connecting');
+
+	setTimeout(this.connect.bind(this), 1 * 1000);
+};
+
+SocketEventClient.prototype.reconnect = function() {
+	if(this.reconnectAttempts++ > 60){
+		this.emit('reconnect_failed');///TODO【闻祖东 2014-8-15-154855】重连失败的地方之后还需要将未Enqueue的数据告知调用方。
+		this.reconnectAttempts = 0;
+		//return;TODO【闻祖东 2014-8-15-155711】这个地方应该也不能return，否则就直接退出了。
+	}
+
+	var sct = this.sckClient.socket;
+
+	if (sct.connected || sct.reconnecting){
+		///TODO【闻祖东 2014-8-15-155017】这个地方需要测试是否不需要做ReEnqueue，SocketIOClient也会自己Enqueue。
+		return;
+	}
 
 	sct.connect();
 	sct.publish('reconnecting');
@@ -32,17 +69,11 @@ SocketEventClient.prototype.reconnect = function() {
 	setTimeout(this.reconnect.bind(this), 1 * 1000);
 };
 
-SocketEventClient.prototype.subscribe = function (eventName, eventArrivedCallback, operationCallback) {
+SocketEventClient.prototype.subscribe = function (eventName) {
 	console.log('已开始订阅事件:', eventName);
 	var subscribeResult = {
 		'evant' : eventName,
 		'operation' : 'subscribe'
-	};
-
-	if(Object.keys(this.eventDic).indexOf(eventName) >= 0){
-		subscribeResult.status = 'ALREADY_SUBSCRIBED';
-
-		return operationCallback(subscribeResult);
 	};
 
 	var arg = {
@@ -53,21 +84,18 @@ SocketEventClient.prototype.subscribe = function (eventName, eventArrivedCallbac
 	
 	this.sckClient.removeAllListeners(eventName);
 	this.sckClient.on(eventName, function(msg){
-		return eventArrivedCallback(msg.args);
-	});
+		this.emit(eventName, msg.args);
+	}.bind(this));
 
 	this.sckClient.emit('subscribe', arg, function(emitResult){
 		subscribeResult.status = emitResult.status;
 
-		if(emitResult.status == 'SUCCESS')
-			this.eventDic[eventName] = eventArrivedCallback;
-
-		return operationCallback(subscribeResult);
+		this.emit('subscribe', subscribeResult);
 	}.bind(this));
 };
 
-SocketEventClient.prototype.enqueue = function (eventName, tryTimes, timeout, params, operationCallback) {
-	console.log('已开始推送事件:', eventName);
+SocketEventClient.prototype.enqueue = function (eventName, tryTimes, timeout, params) {
+	console.log('已开始推送事件(带operationCallback):', eventName);
 	var arg = {
 		'event' : eventName,
 		'senderId' : this.clientId,
@@ -84,33 +112,7 @@ SocketEventClient.prototype.enqueue = function (eventName, tryTimes, timeout, pa
 			'operation' : 'enqueue'
 		};
 
-		operationCallback(enqueueResult);
-	});
-};
-
-SocketEventClient.prototype.reSubscribeEvent = function(eventName, eventArrivedCallback){
-	var arg = {
-		'event' : eventName,
-		'senderId' : this.clientId,
-		'requestId' : this.genGuid()
-	};
-
-	this.sckClient.removeAllListeners(eventName);
-	this.sckClient.on(eventName, function(msg){
-		return eventArrivedCallback(msg.args);
-	});
-
-	this.sckClient.emit('subscribe', arg, function(emitResult){
-		console.log('重新订阅成功，事件:', eventName);
-		return;
-	}.bind(this));
-};
-
-SocketEventClient.prototype.reSubscribeEvents = function(){
-	var callbacks = Object.keys(this.eventDic);
-
-	callbacks.forEach(function(eventName){
-		this.reSubscribeEvent(eventName, this.eventDic[eventName]);
+		this.emit('enqueue', enqueueResult);
 	}.bind(this));
 };
 
@@ -120,6 +122,56 @@ SocketEventClient.prototype.genGuid = function() {
     }; 
 
     return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4()); 
+};
+
+SocketEventClient.prototype.reSubscribeEvents = function(){
+	var self = this;
+	function subscribeOne(evtName){
+		var arg = {
+			'event' : evtName,
+			'senderId' : self.clientId,
+			'requestId' : self.genGuid()
+		};
+
+		self.sckClient.emit('subscribe', arg, function(emitResult){
+			console.log('重新订阅成功，事件:', evtName);
+			return;
+		});
+	};
+
+	this.events.forEach(function(eventName){
+		subscribeOne(eventName);
+	});
+};
+
+
+
+SocketEventClient.prototype.onEventArrived = function(eventName, callback) {
+	if(this.events.indexOf(eventName) < 0)
+		this.events.push(eventName);
+
+	this.addListener(eventName, callback);
+	console.log('订阅事件' + eventName + '完成');
+};
+
+SocketEventClient.prototype.onSubscribeCompleted = function(callback){
+	this.addListener('subscribe', callback);
+	console.log('订阅操作事件添加完成');
+};
+
+SocketEventClient.prototype.onEnqueueCompleted = function(callback){
+	this.addListener('enqueue', callback);
+	console.log('推送操作事件添加完成');
+};
+
+SocketEventClient.prototype.onConnectFailed = function(callback){
+	this.addListener('connect_failed', callback);
+	console.log('连接失败事件添加完成');
+};
+
+SocketEventClient.prototype.onInnerError = function(callback){
+	this.addListener('reconnect_failed', callback);
+	console.log('重连失败(内部错误)事件添加完成');
 };
 
 exports.SocketEventClient = SocketEventClient;
